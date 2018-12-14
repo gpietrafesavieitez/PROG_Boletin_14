e999e1053ee63e9e59d4b367367cb214c2682500
BuildTools <unconfigured@null.spigotmc.org>
2018-12-14 20:59:44 +0100

    Acabar

--- Boletin_14_1/src/boletin_14_1/Boletin_14_1.java
+++ Boletin_14_1/src/boletin_14_1/Boletin_14_1.java
@@ -3,16 +3,21 @@ package boletin_14_1;
 import javax.swing.JOptionPane;
 
 public class Boletin_14_1 {
+    private static ConversorTemperaturas conversor = new ConversorTemperaturas();
     
     public static void main(String[] args) {
-        ConversorTemperaturas conversor = new ConversorTemperaturas();
-        float t = Float.parseFloat(JOptionPane.showInputDialog(null,"Introduzca temperatura (ºC):","Conversor",3));
-        try{
-            JOptionPane.showMessageDialog(null,"- " + conversor.centigradosAFahrenheit(t) + " F\n- " + conversor.centigradosAReamur(t) + " R","Conversor",1);
-        }catch(TemperaturaErradaExcepcion e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println("Fin do programa");
+        float t = 0;
+        do{
+            try{
+                t = Float.parseFloat(JOptionPane.showInputDialog(null,"Introduzca temperatura (ºC):","Conversor",3));
+                JOptionPane.showMessageDialog(null,"- Grados Fahrenheit: " + conversor.centigradosAFahrenheit(t) + "ºF\n- Grados Réaumur: " + conversor.centigradosAReamur(t) + "ºR","Conversor",1);
+            }catch(NullPointerException e){
+                System.exit(0);
+            }catch(TemperaturaErradaExcepcion e1){
+                JOptionPane.showMessageDialog(null,e1.getMessage(),"Conversor",0);
+            }catch(NumberFormatException e2){
+                JOptionPane.showMessageDialog(null,"Formato incorrecto.","Conversor",0);
+            }
+        }while(t < 80);
     }
-    
 }
--- Boletin_14_1/src/boletin_14_1/TemperaturaErradaExcepcion.java
+++ Boletin_14_1/src/boletin_14_1/TemperaturaErradaExcepcion.java
@@ -9,5 +9,4 @@ public class TemperaturaErradaExcepcion extends Exception {
     public TemperaturaErradaExcepcion(String mensaxe) {
         super(mensaxe);
     }
-    
 }
